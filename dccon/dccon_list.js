0910cea403671bc47afa436cc30675ea90191412
Chepyu <33060456+Chepyu@users.noreply.github.com>
2018-08-06 01:17:11 +0900

    Update dccon_list.js

--- dccon/dccon_list.js
+++ dccon/dccon_list.js
@@ -32,7 +32,7 @@ dcConsData = [
 	{ "name": "알파팩.gif", "keywords": ["알파팩"], "tags": ["시지", "움짤"] },
 
 	{ "name": "냥캣.gif", "keywords": ["냥캣"], "tags": ["고양이", "냥캣", "움짤"] },
-   	{ "name": "amd.png", "keywords": ["amd", "암드"], "tags": ["암드", "투명"] }
+   	{ "name": "amd.png", "keywords": ["amd", "암드"], "tags": ["암드", "투명"] },
 
         { "name": "세야나.gif", "keywords": ["세야나"], "tags": ["아카네", "에어케미", "체퓨"] },
         { "name": "그렇구나.gif", "keywords": ["글쿠나", "그렇구나"], "tags": ["아카네", "에어케미", "체퓨"] },
