479715112ad73f13cfd411acf29e3905896c38b4
chepyu <chepyu@naver.com>
2018-08-11 21:03:26 +0900

    20180811
    yup

--- dccon/dccon_list.js
+++ dccon/dccon_list.js
@@ -32,10 +32,42 @@ dcConsData = [
 	{ "name": "알파팩.gif", "keywords": ["알파팩"], "tags": ["시지", "움짤"] },
 
 	{ "name": "냥캣.gif", "keywords": ["냥캣"], "tags": ["고양이", "냥캣", "움짤"] },
-   	{ "name": "amd.png", "keywords": ["amd", "암드"], "tags": ["암드", "투명"] }
+   	{ "name": "amd.png", "keywords": ["amd", "암드"], "tags": ["암드", "투명"] },
 
-        { "name": "세야나.gif", "keywords": ["세야나"], "tags": ["아카네", "에어케미", "체퓨"] },
-        { "name": "그렇구나.gif", "keywords": ["글쿠나", "그렇구나"], "tags": ["아카네", "에어케미", "체퓨"] },
-        { "name": "와카루.gif", "keywords": ["와카루"], "tags": ["아카네", "에어케미", "체퓨"] },
-        { "name": "알긋다.gif", "keywords": ["알긋다", "알겠다"], "tags": ["아카네", "에어케미", "체퓨"] }
+	{ "name": "세야나.gif", "keywords": ["세야나"], "tags": ["아카네", "에어케미", "체퓨"] },
+    { "name": "그렇구나.gif", "keywords": ["글쿠나", "그렇구나"], "tags": ["아카네", "에어케미", "체퓨"] },
+    { "name": "와카루.gif", "keywords": ["와카루"], "tags": ["아카네", "에어케미", "체퓨"] },
+    { "name": "알긋다.gif", "keywords": ["알긋다", "알겠다"], "tags": ["아카네", "에어케미", "체퓨"] },
+	
+	{ "name": "볼트고인.png", "keywords": ["볼트고인"], "tags": ["볼트"] },
+	{ "name": "볼트공중.png", "keywords": ["볼트공중"], "tags": ["볼트"] },
+	{ "name": "볼트구토.png", "keywords": ["볼트구토"], "tags": ["볼트"] },
+	{ "name": "볼트넷여친.png", "keywords": ["볼트넷여친"], "tags": ["볼트"] },
+	{ "name": "볼트넷친구.png", "keywords": ["볼트넷친구"], "tags": ["볼트"] },
+	{ "name": "볼트딴딴.png", "keywords": ["볼트딴딴"], "tags": ["볼트"] },
+	{ "name": "볼트루저.png", "keywords": ["볼트루저"], "tags": ["볼트"] },
+	{ "name": "볼트몸상태.png", "keywords": ["볼트몸상태"], "tags": ["볼트"] },
+	{ "name": "볼트미사일.png", "keywords": ["볼트미사일"], "tags": ["볼트"] },
+	{ "name": "볼트민첩.png", "keywords": ["볼트민첩"], "tags": ["볼트"] },
+	{ "name": "볼트부활.png", "keywords": ["볼트부활"], "tags": ["볼트"] },
+	{ "name": "볼트빠따.png", "keywords": ["볼트빠따"], "tags": ["볼트"] },
+	{ "name": "볼트수류탄.png", "keywords": ["볼트수류탄"], "tags": ["볼트"] },
+	{ "name": "볼트신문.png", "keywords": ["볼트신문"], "tags": ["볼트"] },
+	{ "name": "볼트심리.png", "keywords": ["볼트심리"], "tags": ["볼트"] },
+	{ "name": "볼트에쎔.png", "keywords": ["볼트에쎔"], "tags": ["볼트"] },
+	{ "name": "볼트연설.png", "keywords": ["볼트연설"], "tags": ["볼트"] },
+	{ "name": "볼트우승.png", "keywords": ["볼트우승"], "tags": ["볼트"] },
+	{ "name": "볼트자본.png", "keywords": ["볼트자본"], "tags": ["볼트"] },
+	{ "name": "볼트자살.png", "keywords": ["볼트자살"], "tags": ["볼트"] },
+	{ "name": "볼트장전.png", "keywords": ["볼트장전"], "tags": ["볼트"] },
+	{ "name": "볼트저격.png", "keywords": ["볼트저격"], "tags": ["볼트"] },
+	{ "name": "볼트전기톱.png", "keywords": ["볼트전기톱"], "tags": ["볼트"] },
+	{ "name": "볼트토끼.png", "keywords": ["볼트토끼"], "tags": ["볼트"] },
+	{ "name": "볼트파오후.png", "keywords": ["볼트파오후"], "tags": ["볼트"] },
+	{ "name": "볼트펄럭.png", "keywords": ["볼트펄럭"], "tags": ["볼트"] },
+	{ "name": "볼트화염병.png", "keywords": ["볼트화염병"], "tags": ["볼트"] },
+	{ "name": "볼트활활.png", "keywords": ["볼트활활"], "tags": ["볼트"] },
+	{ "name": "볼트활활2.png", "keywords": ["볼트활활2"], "tags": ["볼트"] }
+	
+	
 ];
